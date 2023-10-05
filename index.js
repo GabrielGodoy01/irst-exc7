9d5d85983b1750028aaac01057e7030d6af55088
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-10-04 21:39:12 -0300

    delete and put

--- index.js
+++ index.js
@@ -26,6 +26,45 @@ livrosRouter.post("/", (req, res) => {
   res.status(201).json(livro);
 });
 
+livrosRouter.put("/", (req, res) => {
+  const livro = {
+    id: req.body.id,
+    titulo: req.body.titulo,
+    descricao: req.body.descricao,
+    autor: req.body.autor,
+  };
+
+  try {
+    const index = livros.findIndex((e) => {
+      return e.id === livro.id
+    });
+  
+    livros[index] = livro;
+
+  } catch (error) {
+    res.status(400).json(error)
+  }
+
+  res.status(200).json(livro);
+})
+
+livrosRouter.delete("/", (req, res) => {
+  id = req.body.id
+
+  try {
+
+    const index = livros.findIndex((e) => {
+      return e.id === id
+    })
+  
+    livros.delete(index)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+
+  res.status(200).json('Livro deletado');
+})
+
 
 const app = express();
 app.use(express.json())
